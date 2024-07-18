90f3c8c87f6041f28683d0feee084d9f34f45ed2
duasenge <brummelmayano@gmail.com>
2024-07-18 19:02:47 +0100

    Fonction pour effectuer l'OCR avec PaddleOCR

---
+++ ocr_paddle.py
@@ -0,0 +1,29 @@
+from paddleocr import PaddleOCR, draw_ocr
+
+def ocr_paddle(image_path):
+  """
+  Fonction pour effectuer l'OCR avec PaddleOCR
+  
+  Args:
+    image_path (str): image ou Chemin vers l'image à OCR
+  
+  Returns:
+    values_list (list): Liste des valeurs extraites
+  
+  """
+
+  # Initialiser PaddleOCR
+  ocr = PaddleOCR(use_angle_cls=True, lang='en')
+
+  # Effectuer l'OCR
+  result = ocr.ocr(image_path, cls=True)
+
+  # Récupérer le texte si le score est > 70% 
+  values_list=[]
+  for line in result:
+      for word_info in line:
+        if word_info[1][1]>0.7:
+          values_list.append(word_info[1][0])
+  
+  return values_list
+  
