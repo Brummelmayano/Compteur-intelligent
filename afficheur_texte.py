9c2a031106aa64d15ba99fbc23b3f31a5e300733
duasenge <brummelmayano@gmail.com>
2024-12-20 19:20:48 +0100

    mise à jour de la fcoonctionnalité de calibrage

--- afficheur_texte.py
+++ afficheur_texte.py
@@ -8,7 +8,7 @@ from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
 from luma.core.legacy import text, textsize, show_message
-from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT, SEG7_FONT, BCD_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT, SEG7_FONT, BCD_FONT, CP437_FONT, TOM_THUMB_FONT
+from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 from luma.core.virtual import viewport
 
 
