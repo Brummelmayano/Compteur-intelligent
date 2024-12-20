9e704dfa5812763d9fc1b0e4d28b3dd01873334e
duasenge <brummelmayano@gmail.com>
2024-12-20 20:11:40 +0100

    mise a jour de la fonction starter

--- starter.py
+++ starter.py
@@ -30,22 +30,31 @@ def starter():
     # Étape 1 : Récupération du périphérique vidéo
     device_path = find_device_path()
     if device_path is None:
-        afficheur.mettre_a_jour_texte("Aucune source video disponible")
+        afficheur.mettre_a_jour_texte("Aucune source vidéo disponible")
         return
 
     # Étape 2 : Capture d'une image depuis le périphérique trouvé
     image_frame = capture_image(device_path)
 
-    # Étape 3 : Vérification de la similarité avec les bruits
-    resultat = verifier_similarite_frame(repertoire_images_reference, image_frame, seuil=0.8)
-    
-    if resultat:  # L'image capturée est similaire aux bruits
-        afficheur.mettre_a_jour_texte("Verifier le cable HDMI")
+    # Vérification de l'image capturée
+    if image_frame is None or image_frame.size == 0:
+        afficheur.mettre_a_jour_texte("Erreur : Aucune image capturée. Vérifiez le périphérique vidéo.")
+        return
 
+    # Étape 3 : Vérification de la similarité avec les bruits
+    try:
+        resultat = verifier_similarite_frame(repertoire_images_reference, image_frame, seuil=0.8)
+    except Exception as e:
+        afficheur.mettre_a_jour_texte(f"Erreur : {e}")
+        return
+
+    if resultat:  # L'image capturée est similaire aux bruits
+        afficheur.mettre_a_jour_texte("Vérifier le câble HDMI")
     else:  # L'image est claire, on lance le programme principal
         afficheur.mettre_a_jour_texte("Lancement du programme...")
-        import main  
+        import main
         main.run()
+
 
 if __name__ == "__main__":
     starter()
