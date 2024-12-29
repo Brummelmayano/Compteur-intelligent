7d526a1317a698858bd38483d17da998911a8221
duasenge <brummelmayano@gmail.com>
2024-12-29 13:05:07 +0100

    update

--- starter.py
+++ starter.py
@@ -12,7 +12,7 @@ def starter(afficheur):
         # Étape 1 : Récupération du périphérique vidéo
         device_path = find_device_path()
         if device_path is None:
-            afficheur.mettre_a_jour_texte("Pas de source vidéo")
+            afficheur.mettre_a_jour_texte("PAS DE SOURCE VIDEO")
             return
 
         # Étape 2 : Capture d'une image depuis le périphérique trouvé
@@ -20,29 +20,33 @@ def starter(afficheur):
 
         # Vérification de l'image capturée
         if image_frame is None or image_frame.size == 0:
-            afficheur.mettre_a_jour_texte("Erreur : Image non capturée")
+            print("Erreur : Image non capturée")
             return
+        else:
+            afficheur.mettre_a_jour_texte("LANCEMENT...")
+
+            
 
         # Étape 3 : Vérification de la similarité avec les bruits
         try:
             resultat = verifier_similarite_frame(repertoire_images_reference, image_frame, seuil=0.8)
         except Exception as e:
-            afficheur.mettre_a_jour_texte(f"Erreur : {e}")
+            print(f"Erreur : {e}")
             return
 
         # Étape 4 : Actions en fonction du résultat
         if resultat:
-            afficheur.mettre_a_jour_texte("Vérifier câble HDMI")
+            afficheur.mettre_a_jour_texte("VERIFIER LA SOURCE VIDEO")
         else:
-            afficheur.mettre_a_jour_texte("Lancement du programme...")
+            afficheur.mettre_a_jour_texte("LANCEMENT...")
             import main
             main.run()
 
     except Exception as e:
-        afficheur.mettre_a_jour_texte(f"Erreur inattendue : {e}")
+        print(f"Erreur inattendue : {e}")
 
 if __name__ == "__main__":
-    afficheur = AfficheurTexte(cascaded=4, mode_bouton=1)
+    afficheur = AfficheurTexte(cascaded=2, mode_bouton=1)
     #afficheur.demarrer()  # Démarre le thread d'affichage
 
     while True:
