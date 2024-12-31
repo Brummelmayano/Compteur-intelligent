32ee1f5e3e66fea0d9257a6b0133a1dd7f424744
duasenge <brummelmayano@gmail.com>
2024-12-31 17:42:26 +0100

    commenter l'appel à la fonction main dans starter.py pour éviter son exécution

--- starter.py
+++ starter.py
@@ -44,8 +44,8 @@ def starter(afficheur):
             #afficheur.terminer_processus_spi()
             #afficheur.arreter()
 
-            from main import main
-            main()
+            #from main import main
+            #main()
 
     except Exception as e:
         print(f"Erreur inattendue : {e}")
